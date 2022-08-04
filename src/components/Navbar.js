3d9b683c24bed618570e1f390805ac3732d3eaeb
killyvera <david.esmeralda@gmail.com>
2022-08-04 14:59:14 -0500

    solved navbar responsive logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,13 +5,13 @@ import { Stack } from '@mui/material';
 import Logo from '../assets/images/Logo.png';
 
 const Navbar = () => (
-  <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px">
+  <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '20px', xs: '25px' }, mt: { sm: '40px', xs: '20px' }, justifyContent: 'left' }} px="20px">
     <Link to="/">
       <img src={Logo} alt="logo" style={{ width: '87px', margin: '0px 20px' }} />
     </Link>
     <Stack
       direction="row"
-      gap="40px"
+      gap="20px"
       fontFamily="Alegreya"
       fontSize="24px"
       alignItems="flex-end"
