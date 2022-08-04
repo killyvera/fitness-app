0e1184c14fdfe03fdb230cc76c3337223d18b227
killyvera <david.esmeralda@gmail.com>
2022-08-04 06:35:26 -0500

    add components and pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import './App.css';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
-export default App
+const App = () => (
+  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+  </Box>
+);
 
-//rafce
+export default App;
