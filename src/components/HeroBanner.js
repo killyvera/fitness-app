c68c5662f07d796c9f69b443fd07a8749f0fc6ed
killyvera <david.esmeralda@gmail.com>
2022-08-04 11:57:52 -0500

    modified some design topics

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -5,8 +5,8 @@ import HeroBannerImage from '../assets/images/banner.png';
 
 const HeroBanner = () => (
   <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
-    <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
-    <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
+    <Typography color="#FF2625" fontWeight="600" fontSize="34px">The Fitness App</Typography>
+    <Typography fontWeight={700} sx={{ fontSize: { lg: '48px', xs: '44px' } }} mb="23px" mt="30px">
       Sweat, Smile <br />
       And Repeat
     </Typography>
